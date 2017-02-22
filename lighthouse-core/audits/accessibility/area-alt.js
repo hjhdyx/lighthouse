67508a45060c6bd7d2fbfc27ee952b3a07b098f8
Rob Dodson <robdodson@google.com>
2017-02-21 17:51:16 -0800

    work-in-progress

---
+++ lighthouse-core/audits/accessibility/area-alt.js
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+/**
+ * @fileoverview Ensures `<area>` elements of image maps have alternate text.
+ * See base class in axe-audit.js for audit() implementation.
+ */
+
+const AxeAudit = require('./axe-audit');
+
+class AreaAlt extends AxeAudit {
+  /**
+   * @return {!AuditMeta}
+   */
+  static get meta() {
+    return {
+      category: 'Accessibility',
+      name: 'area-alt',
+      description: 'Clickable areas in an image map have alt text.',
+      helpText: 'Screen readers have no way of translating graphics into words. Consider adding' +
+          '`alt` text for clickable areas in an image map, as well as for the larger image.',
+      requiredArtifacts: ['Accessibility']
+    };
+  }
+}
+
+module.exports = AreaAlt;
---
+++ lighthouse-core/audits/accessibility/blink.js
@@ -0,0 +1,42 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+/**
+ * @fileoverview Ensures `<blink>` elements are not used.
+ * See base class in axe-audit.js for audit() implementation.
+ */
+
+const AxeAudit = require('./axe-audit');
+
+class Blink extends AxeAudit {
+  /**
+   * @return {!AuditMeta}
+   */
+  static get meta() {
+    return {
+      category: 'Accessibility',
+      name: 'blink',
+      description: 'No `<blink>` elements are used.',
+      helpText: '`<blink>` elements cause content to flash, making items hard to read or activate.',
+      requiredArtifacts: ['Accessibility']
+    };
+  }
+}
+
+module.exports = Blink;
